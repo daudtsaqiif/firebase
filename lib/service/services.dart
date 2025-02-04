bf18df002313f61335afd9bedbac1c161690fa7e
daudtsaqiif <daudtsaqiif49@gmail.com>
2025-02-04 08:24:36 +0700

    21

--- lib/service/services.dart
+++ lib/service/services.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
@@ -22,7 +23,8 @@ class FirebaseService {
 
   Future<User?> signUp(String email, String password) async {
     try {
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
@@ -41,4 +43,10 @@ class FirebaseService {
   }
 
   get currentUser => _auth.currentUser;
+
+  Future<Map<String, dynamic>> getUserData(String userId) async {
+    final DocumentSnapshot userDoc =
+        await FirebaseFirestore.instance.collection("users").doc(userId).get();
+    return userDoc.data() as Map<String, dynamic>;
+  }
 }
--- lib/ui/ui/home_page.dart
+++ lib/ui/ui/home_page.dart
@@ -8,43 +8,119 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? userId;
+  late Future<Map<String, dynamic>> userData;
 
-  FirebaseService _auth = FirebaseService();
+  final FirebaseService _auth = FirebaseService();
 
-void _signOut() async {
-  await _auth.signOut();
-  Navigator.pushReplacementNamed(context, '/sign-in');
-}
+  @override
+  void initState() {
+    super.initState();
 
+    User? user = _auth.currentUser;
+
+    userId = user!.uid;
+    userData = _auth.getUserData(userId!);
+  }
+
+  void _signOut() async {
+    await _auth.signOut();
+    Navigator.pushReplacementNamed(context, '/sign-in');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              welcomeBackText,
-              style: welcomeTextStyle,
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: userData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text('Error fetching data'));
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('User data not found'));
+          }
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  welcomeText,
+                  style: welcomeTextStyle,
+                ),
+                Text(
+                  "Email: ${snapshot.data!['email']}",
+                  style: subWelcomeTextStyle.copyWith(fontSize: 16),
+                ),
+                Text(
+                  'name: ${snapshot.data!['first_name']}',
+                  style: subWelcomeTextStyle,
+                ),
+                Text(
+                  'uid: $userId',
+                  style: subWelcomeTextStyle,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Attedance',
+                        style: TextStyle(color: colorWhite),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Notes',
+                        style: TextStyle(color: colorWhite),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Profile',
+                        style: TextStyle(color: colorWhite),
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _signOut();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: Text(
+                    "Sign Out",
+                    style: TextStyle(
+                      color: colorWhite,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Email saya: ${_auth.currentUser!.email}',
-              style: subWelcomeTextStyle.copyWith(fontSize: 16),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _signOut();
-              },
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-              child: Text(
-                'logout',
-                style: TextStyle(color: colorWhite),
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
