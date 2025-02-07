c2562c3a8f1421e8a79a55a5e48736aa11e62355
daudtsaqiif <daudtsaqiif49@gmail.com>
2025-02-07 11:37:49 +0700

    1

--- lib/ui/ui/change_password_page.dart
+++ lib/ui/ui/change_password_page.dart
@@ -8,6 +8,11 @@ class ChangePasswordPage extends StatefulWidget {
 }
 
 class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  bool isObscureText = true;
+  TextEditingController currentPasswordController = TextEditingController();
+  TextEditingController NewPasswordController = TextEditingController();
+  TextEditingController ConfrimPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +20,111 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
         title: Text('Change Password'),
       ),
       body: Center(
-        child: Text('Change Password Page'),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: TextFormField(
+                controller: currentPasswordController,
+                decoration: InputDecoration(
+                  hintText: 'Current Password',
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscureText = !isObscureText;
+                      });
+                    },
+                    icon: isObscureText
+                        ? Icon(
+                            Icons.visibility_off_outlined,
+                            color: colorPrimary,
+                          )
+                        : Icon(
+                            Icons.visibility,
+                            color: colorPrimary,
+                          ),
+                  ),
+                ),
+                obscureText: isObscureText ? true : false,
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: TextFormField(
+                controller: NewPasswordController,
+                decoration: InputDecoration(
+                  hintText: 'New Password',
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscureText = !isObscureText;
+                      });
+                    },
+                    icon: isObscureText
+                        ? Icon(
+                            Icons.visibility_off_outlined,
+                            color: colorPrimary,
+                          )
+                        : Icon(
+                            Icons.visibility,
+                            color: colorPrimary,
+                          ),
+                  ),
+                ),
+                obscureText: isObscureText ? true : false,
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: TextFormField(
+                controller: ConfrimPasswordController,
+                decoration: InputDecoration(
+                  hintText: 'confrim password',
+                  prefixIcon: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isObscureText = !isObscureText;
+                      });
+                    },
+                    icon: isObscureText
+                        ? Icon(
+                            Icons.visibility_off_outlined,
+                            color: colorPrimary,
+                          )
+                        : Icon(
+                            Icons.visibility,
+                            color: colorPrimary,
+                          ),
+                  ),
+                ),
+                obscureText: isObscureText ? true : false,
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text('Save')),
+          ],
+        ),
       ),
     );
   }
