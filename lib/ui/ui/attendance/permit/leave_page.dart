bdefacfdd25e7f4797050e037f02f95cef51f708
daudtsaqiif <daudtsaqiif49@gmail.com>
2025-03-07 13:34:57 +0700

    1

--- lib/ui/ui/attendance/permit/leave_page.dart
+++ lib/ui/ui/attendance/permit/leave_page.dart
@@ -18,7 +18,7 @@ class _LeavePageState extends State<LeavePage> {
   double dLong = 0.0;
   final controllerName = TextEditingController();
   final fromController = TextEditingController();
-  final toController = TextEditingController();
+  final untilController = TextEditingController();
   String dropValueCategories = "Please Choose";
   var caetgoryList = <String>[
     "Please Choose",
@@ -60,30 +60,9 @@ class _LeavePageState extends State<LeavePage> {
     }
   }
 
-  showLoaderDialog(BuildContext context) {
-    AlertDialog alertDialog = AlertDialog(
-      content: Row(
-        children: <Widget>[
-          const CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 7),
-            child: const Text("Please Wait..."),
-          ),
-        ],
-      ),
-    );
-    showDialog(
-        barrierDismissible: false,
-        context: context,
-        builder: (BuildContext context) {
-          return alertDialog;
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Leave page'),
@@ -186,12 +165,17 @@ class _LeavePageState extends State<LeavePage> {
                               child: TextField(
                                 controller: fromController,
                                 onTap: () async {
-                                  DateTime? pickDateTime = await showDatePicker(
+                                  DateTime? pickedDate = await showDatePicker(
                                     context: context,
                                     initialDate: DateTime.now(),
                                     firstDate: DateTime(1900),
                                     lastDate: DateTime(9999),
                                   );
+                                  if (pickedDate != null) {
+                                    fromController.text =
+                                        DateFormat('dd/M/yyyy')
+                                            .format(pickedDate);
+                                  }
                                 },
                               ),
                             ),
@@ -205,14 +189,19 @@ class _LeavePageState extends State<LeavePage> {
                             const SizedBox(width: 10),
                             Expanded(
                               child: TextField(
-                                controller: fromController,
+                                controller: untilController,
                                 onTap: () async {
-                                  DateTime? pickDateTime = await showDatePicker(
+                                  DateTime? pickedDate = await showDatePicker(
                                     context: context,
                                     initialDate: DateTime.now(),
                                     firstDate: DateTime(1900),
                                     lastDate: DateTime(9999),
                                   );
+                                  if (pickedDate != null) {
+                                    untilController.text =
+                                        DateFormat('dd/M/yyyy')
+                                            .format(pickedDate);
+                                  }
                                 },
                               ),
                             ),
@@ -222,11 +211,176 @@ class _LeavePageState extends State<LeavePage> {
                     ],
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(8),
+                    child: Material(
+                      elevation: 3,
+                      borderRadius: BorderRadius.circular(30),
+                      child: Container(
+                        width: size.width * 0.8,
+                        height: 48,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.amber,
+                        ),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.blueAccent,
+                          child: InkWell(
+                            splashColor: Colors.blue,
+                            borderRadius: BorderRadius.circular(20),
+                            onTap: () {
+                              if (controllerName.text.isEmpty ||
+                                  dropValueCategories == "Please Choose" ||
+                                  fromController.text.isEmpty ||
+                                  untilController.text.isEmpty) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Row(
+                                      children: [
+                                        Icon(Icons.info_outline,
+                                            color: Colors.white),
+                                        SizedBox(width: 10),
+                                        Text(
+                                          "Please Fill all the form",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                    backgroundColor: Colors.blueAccent,
+                                    shape: StadiumBorder(),
+                                    behavior: SnackBarBehavior.floating,
+                                  ),
+                                );
+                              } else {
+                                submitAbsent(
+                                    controllerName.text.toString(),
+                                    dropValueCategories.toString(),
+                                    fromController.text,
+                                    untilController.text);
+                              }
+                            },
+                            child: Center(
+                              child: Text(
+                                "Submit",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> submitAbsent(
+      String name, String note, String from, String until) async {
+    showLoaderDialog(context);
+
+    String userId = FirebaseAuth.instance.currentUser?.uid ?? 'Unknown';
+    print("USER ID =  $userId");
+    if (userId == 'Unknown') {
+      print("ERROR: User ID is unknowner");
+      return;
+    }
+
+    DocumentReference UserDocRef = firestore.collection('users').doc(userId);
+    CollectionReference attendanceCollection =
+        UserDocRef.collection('attendance');
+
+    attendanceCollection.add({
+      'name': name,
+      'description': note,
+      'dateTime': '$from-$until',
+      'createdAt': FieldValue.serverTimestamp(),
+    }).then(
+      (result) {
+        print("Data has been saved whit ID = ${result.id}");
+        setState(
+          () {
+            Navigator.of(context).pop();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Row(
+                  children: [
+                    Icon(Icons.check_circle_outline, color: Colors.white),
+                    SizedBox(width: 10),
+                    Text(
+                      "Data has been saved",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+                backgroundColor: Colors.greenAccent,
+                behavior: SnackBarBehavior.floating,
+                shape: StadiumBorder(),
+                duration: Duration(seconds: 3),
+              ),
+            );
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomeAttendancePage(),
+              ),
+            );
+          },
+        );
+      },
+    ).catchError((error) {
+      print("filed to save data: $error");
+      Navigator.of(context).pop();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: [
+              Icon(Icons.check_circle_outline, color: Colors.white),
+              SizedBox(width: 10),
+              Text(
+                "Upss...$error",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          backgroundColor: Colors.blueGrey,
+          behavior: SnackBarBehavior.floating,
+          shape: StadiumBorder(),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    });
+  }
+
+  showLoaderDialog(BuildContext context) {
+    AlertDialog alertDialog = AlertDialog(
+      content: Row(
+        children: <Widget>[
+          const CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 7),
+            child: const Text("Please Wait..."),
+          ),
+        ],
+      ),
+    );
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
   }
 }
