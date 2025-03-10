f38e90136266a594babd345874ac5d394ccce523
daudtsaqiif <daudtsaqiif49@gmail.com>
2025-03-10 11:21:48 +0700

    pp

--- lib/ui/ui/attendance/history/history_Page.dart
+++ lib/ui/ui/attendance/history/history_Page.dart
@@ -10,6 +10,37 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('History'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
+
+class DataServiceHistoryAttendance {
+  final auth = FirebaseAuth.instance;
+
+  //get id attendance users
+
+  CollectionReference getUserAttendance() {
+    final String? userId = auth.currentUser?.uid;
+    if (userId == null) {
+      throw Exception("User is not logged in");
+    }
+    return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('attendance');
+  }
+
+  Stream<QuerySnapshot> getAttendanceStream() {
+    return getUserAttendance().orderBy('createdAt', descending: true).snapshots();
   }
 }
